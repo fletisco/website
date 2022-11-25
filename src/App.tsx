3f4f0211554122515029055d7737cc0ba069098b
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-11-25 19:36:20 +0530

    update website

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="main">
+      <p className="title">Fletisco</p>
+      <h1 className="header-text">THE ADVENTURE BEGINS</h1>
+      <p className="header-paragraph">
+        We are a small and growing development team with big ideas
+      </p>
+      <a href="https://github.com/fletisco" className="button-51">
+        Learn More
+      </a>
+    </div>
+  );
+}
+
+export default App;
